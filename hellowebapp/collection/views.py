28013b9d0b9c81c6e144cc291535debfdc2b4053
James Quinlan <quinlanjc@vcu.edu>
2016-02-27 23:44:00 -0500

    explore templating

--- hellowebapp/collection/views.py
+++ hellowebapp/collection/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    number = 9
     # This is a new view for the 'home' route defined in
     # urls.py of the project folder, next to settings.py
-    return render(request, 'index.html');
+    return render(request, 'index.html', { 'number': number })
