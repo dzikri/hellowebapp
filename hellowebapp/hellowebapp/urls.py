501ec0b37e7c777e5aeeaf8281d47bed23770124
James Quinlan <quinlanjc@vcu.edu>
2016-03-01 23:46:45 -0500

    add password change and reset

--- hellowebapp/hellowebapp/urls.py
+++ hellowebapp/hellowebapp/urls.py
@@ -24,5 +24,9 @@ urlpatterns = [
     url(r'^quote/(?P<slug>[-\w]+)/$', 'collection.views.quote_detail', name='quote_detail'),
     url(r'^quote/(?P<slug>[-\w]+)/edit/$', 'collection.views.edit_quote', name='edit_quote'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^admin/', admin.site.urls),    
+    url(r'^accounts/password/reset/$', password_reset, {'template_name': 'registration/password_reset_form.html'}, name='password_reset'),
+    url(r'^accounts/password/reset/done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name='password_reset_done'),
+    url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z_\-])+/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name='password_reset_confirm'),
+    url(r'^accounts/password/done/$', password_reset_complete, {'template_name': 'registration/password_reset_complete.html'}, name='password_reset_complete'),
+    url(r'^admin/', admin.site.urls),
 ]
