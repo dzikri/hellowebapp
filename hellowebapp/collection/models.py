35833d594d2babe24c9190ef2bb0e867224cd86b
James Quinlan <quinlanjc@vcu.edu>
2016-03-11 22:23:16 -0500

    Add user field to Quote schema

--- hellowebapp/collection/models.py
+++ hellowebapp/collection/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Quote(models.Model):
@@ -6,3 +7,4 @@ class Quote(models.Model):
     text = models.TextField()
     description = models.TextField()
     slug = models.SlugField(unique=True)
+    user = models.OneToOneField(User, blank=True, null=True)
--- hellowebapp/hellowebapp/settings.py
+++ hellowebapp/hellowebapp/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'registration',
+    'registration', # This takes care of all the registration forms and what not
 ]
 
 MIDDLEWARE_CLASSES = [
