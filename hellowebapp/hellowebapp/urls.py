61d61f2df28fa5e2446d9bd2587cf52a71135c6d
James Quinlan <quinlanjc@vcu.edu>
2016-04-01 21:09:09 -0400

    added registration-redux

--- hellowebapp/hellowebapp/urls.py
+++ hellowebapp/hellowebapp/urls.py
@@ -32,7 +32,6 @@ urlpatterns = [
     url(r'^contact/$', TemplateView.as_view(template_name="contact.html"), name='contact'),
     url(r'^quote/(?P<slug>[-\w]+)/$', 'collection.views.quote_detail', name='quote_detail'),
     url(r'^quote/(?P<slug>[-\w]+)/edit/$', 'collection.views.edit_quote', name='edit_quote'),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^accounts/password/reset/$', password_reset, {'template_name': 'registration/password_reset_form.html'}, name='password_reset'),
     url(r'^accounts/password/reset/done/$', password_reset_done, {'template_name': 'registration/password_reset_done.html'}, name='password_reset_done'),
     url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', password_reset_confirm, {'template_name': 'registration/password_reset_confirm.html'}, name='password_reset_confirm'),
